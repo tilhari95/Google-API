52d5c500349eb60a518e8735ab3a6d4383c1a88e
tilhari95 <suyash.tilhari12@gmail.com>
2016-01-17 23:34:05 +0530

    added:java code for implementing Google-api

---
+++ EJB/SourceCode/src/main/java/com/google/api/services/samples/plus/Suyash.java
@@ -0,0 +1,35 @@
+package com.google.api.services.samples.plus;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class Suyash extends HttpServlet {
+   
+    /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+       
+       
+            out.print("<h1>WELCOME TO MY FIRST WEB PAGE</h1>");
+        
+                            out.close();
+            }
+	
+	@Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
